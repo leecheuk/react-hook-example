82d1c7ce01d6c7c6447747bdace2489a43b97c17
m <dev.cheuk.lee@gmail.com>
2019-09-18 16:16:36 -0400

    added useEffect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        alert('You successfully clicked!')
+    }, [count])
 
     return (
         <>
